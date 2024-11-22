b5bbaa752febcb43d0c65de4f839c36ef24a178b
Taylor Rogers <taylor.rogers71@gmail.com>
2024-11-22 14:33:13 -0800

    Increase formatter timeout

--- init.lua
+++ init.lua
@@ -694,7 +694,7 @@ require('lazy').setup({
     opts = {
       stop_after_first = true,
       format_on_save = {
-        timeout_ms = 500,
+        timeout_ms = 2000,
         -- lsp_fallback = true,
       },
       formatters_by_ft = {
