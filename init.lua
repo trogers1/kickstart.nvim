bcd9e1df4a26bd02c0d609ce4799a56a832f9e69
Taylor Rogers <taylor.rogers71@gmail.com>
2025-08-14 10:58:37 -0700

    add json language server

--- init.lua
+++ init.lua
@@ -625,6 +625,7 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
+        jsonls = {},
         pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
